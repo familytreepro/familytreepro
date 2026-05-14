c3661fd52cbea5c945fc3155764246fdb2504928
familytreepro <alibahripor33@gmail.com>
2026-05-14 11:55:47 +0330

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,17 @@
+const VERSION = "v65-force-update";
+
+self.addEventListener("install", event => {
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil((async () => {
+    const keys = await caches.keys();
+    await Promise.all(keys.map(key => caches.delete(key)));
+    await self.clients.claim();
+  })());
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(fetch(event.request, { cache: "no-store" }));
+});
