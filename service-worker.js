6275f2504804251c3fb3e10119bd6353a8f69002
familytreepro <alibahripor33@gmail.com>
2026-05-14 12:06:02 +0330

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,5 @@
+const CACHE_NAME = "familytree-v63-clean-rebuild";
+const urlsToCache = ["./", "./index.html", "./manifest.json"];
+self.addEventListener("install", event => { self.skipWaiting(); event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache).catch(() => true))); });
+self.addEventListener("activate", event => { event.waitUntil(caches.keys().then(keys => Promise.all(keys.map(key => key !== CACHE_NAME ? caches.delete(key) : null))).then(() => self.clients.claim())); });
+self.addEventListener("fetch", event => { if(event.request.method !== "GET") return; event.respondWith(fetch(event.request).then(response => { const copy = response.clone(); caches.open(CACHE_NAME).then(cache => cache.put(event.request, copy)).catch(() => {}); return response; }).catch(() => caches.match(event.request).then(response => response || caches.match("./index.html")))); });
