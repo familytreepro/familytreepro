15a716fc8c95a8064913cee9a9bbd4893127e637
familytreepro <alibahripor33@gmail.com>
2026-05-21 13:07:49 +0330

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,7 @@
-// FamilyTreePro V100.5 Bulk Children Add - safe no-cache service worker
+// FamilyTreePro V100.6 Clean Bulk Children UI - safe no-cache service worker
 self.addEventListener('install', event => { self.skipWaiting(); });
 self.addEventListener('activate', event => { event.waitUntil((async()=>{ try{ const keys=await caches.keys(); await Promise.all(keys.map(k=>caches.delete(k))); }catch(e){} await self.clients.claim(); })()); });
 self.addEventListener('fetch', event => { event.respondWith(fetch(event.request).catch(()=>caches.match(event.request))); });
 
 
-/* cache: v100-5-bulk-children-add */
+/* cache: v100-6-clean-bulk-children-ui */
