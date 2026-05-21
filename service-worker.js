0fabab7c33e3edba409a0c8b05c75e0b079c7d45
familytreepro <alibahripor33@gmail.com>
2026-05-21 20:31:41 +0330

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,12 @@
-// FamilyTreePro V117 Firebase fixed no-cache service worker
+const CACHE_NAME = 'familytreepro-v118-no-cache';
 self.addEventListener('install', event => { self.skipWaiting(); });
-self.addEventListener('activate', event => { event.waitUntil((async()=>{ try{ const keys=await caches.keys(); await Promise.all(keys.map(k=>caches.delete(k))); }catch(e){} await self.clients.claim(); })()); });
-self.addEventListener('fetch', event => { event.respondWith(fetch(event.request, {cache:'no-store'}).catch(()=>caches.match(event.request))); });
+self.addEventListener('activate', event => {
+  event.waitUntil((async () => {
+    const keys = await caches.keys();
+    await Promise.all(keys.map(k => caches.delete(k)));
+    await self.clients.claim();
+  })());
+});
+self.addEventListener('fetch', event => {
+  event.respondWith(fetch(event.request, { cache: 'no-store' }).catch(() => fetch(event.request)));
+});
