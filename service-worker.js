abf1cc0298258b7f0cb22ce70c3514f85d04027c
familytreepro <alibahripor33@gmail.com>
2026-05-21 20:14:53 +0330

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,4 @@
-// FamilyTreePro V116 Firebase Restore - safe no-cache service worker
+// FamilyTreePro V117 Firebase fixed no-cache service worker
 self.addEventListener('install', event => { self.skipWaiting(); });
 self.addEventListener('activate', event => { event.waitUntil((async()=>{ try{ const keys=await caches.keys(); await Promise.all(keys.map(k=>caches.delete(k))); }catch(e){} await self.clients.claim(); })()); });
-self.addEventListener('fetch', event => { event.respondWith(fetch(event.request).catch(()=>caches.match(event.request))); });
-
-/* cache: v116-firebase-restore-safe */
+self.addEventListener('fetch', event => { event.respondWith(fetch(event.request, {cache:'no-store'}).catch(()=>caches.match(event.request))); });
