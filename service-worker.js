d6526971b2b37ad1f626c0846b0ec150a4735389
familytreepro <alibahripor33@gmail.com>
2026-05-23 16:56:01 +0330

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'familytreepro-v137-no-cache';
+const CACHE_NAME = 'familytreepro-v141-no-cache';
 self.addEventListener('install', event => { self.skipWaiting(); });
 self.addEventListener('activate', event => {
   event.waitUntil((async () => {
