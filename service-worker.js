cf70e9b310c748dd161cc33adc36b3cd0a121af5
familytreepro <alibahripor33@gmail.com>
2026-05-27 00:47:40 +0330

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'familytreepro-v148-clean-layer-stable-no-cache';
+const CACHE_NAME = 'familytreepro-v149-photo-ai-safe-no-cache';
 self.addEventListener('install', event => {
   self.skipWaiting();
 });
