f24df23e3bac33daca4fff6f8492a6d060d39928
familytreepro <alibahripor33@gmail.com>
2026-05-27 19:32:42 +0330

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,12 +1,8 @@
-// FamilyTreePro V163 clean stable no-cache
-const VERSION = 'V163-CLEAN-STABLE';
+// FamilyTreePro V164 clean outputs active - no cache
+const VERSION = 'V164-CLEAN-OUTPUT-ACTIVE';
 self.addEventListener('install', event => self.skipWaiting());
 self.addEventListener('activate', event => {
-  event.waitUntil(
-    caches.keys()
-      .then(keys => Promise.all(keys.map(k => caches.delete(k))))
-      .then(() => self.clients.claim())
-  );
+  event.waitUntil(caches.keys().then(keys => Promise.all(keys.map(k => caches.delete(k)))).then(() => self.clients.claim()));
 });
 self.addEventListener('fetch', event => {
   event.respondWith(fetch(event.request, {cache:'no-store'}));
