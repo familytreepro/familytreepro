6012a47f954c4e271e4036ea1836a93b422ff116
familytreepro <alibahripor33@gmail.com>
2026-05-27 19:10:43 +0330

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,11 +1,8 @@
-// V160_SPOUSE_OUTSIDER_FORCE_2026_05_27
-// Safe no-cache service worker for GitHub Pages
+// FamilyTreePro V162 clean no-cache
 self.addEventListener('install', event => self.skipWaiting());
 self.addEventListener('activate', event => {
-  event.waitUntil(
-    caches.keys().then(keys => Promise.all(keys.map(k => caches.delete(k)))).then(() => self.clients.claim())
-  );
+  event.waitUntil(caches.keys().then(keys => Promise.all(keys.map(k => caches.delete(k)))).then(() => self.clients.claim()));
 });
 self.addEventListener('fetch', event => {
-  event.respondWith(fetch(event.request).catch(() => new Response('', {status: 503, statusText: 'offline'})));
+  event.respondWith(fetch(event.request));
 });
