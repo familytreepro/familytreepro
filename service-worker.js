e484354f37756eed17676fb11d555766b1873b15
familytreepro <alibahripor33@gmail.com>
2026-05-29 01:01:56 +0330

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 // FamilyTreePro V153 Security Layer - no-cache
-const CACHE_NAME = 'familytreepro-v154-children-manager-fix-no-cache';
+const CACHE_NAME = 'familytreepro-v155-captcha-fix-no-cache';
 self.addEventListener('install', event => self.skipWaiting());
 self.addEventListener('activate', event => {
   event.waitUntil(caches.keys().then(keys => Promise.all(keys.map(k => caches.delete(k)))).then(() => self.clients.claim()));
