4c41cade0c1d7a3699dd8337a9c0ccaec692229b
familytreepro <alibahripor33@gmail.com>
2026-05-29 11:26:40 +0330

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-// FamilyTreePro V156.7 Children Search Fixed - no-cache
-const CACHE_NAME = 'familytreepro-v156-7-children-search-fixed-no-cache';
+// FamilyTreePro V156 Clean Final - no-cache
+const CACHE_NAME = 'familytreepro-v156-5-children-drawer-clean-no-cache';
 
 self.addEventListener('install', event => {
   self.skipWaiting();
