526308952a0ab143eb6e5d110f4243275574557e
familytreepro <alibahripor33@gmail.com>
2026-05-30 14:18:59 +0330

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-// FamilyTreePro V156.25 Children Select Fix - no-cache
-const CACHE_NAME = 'familytreepro-v156-25-children-select-fix-no-cache';
+// FamilyTreePro V156.26 Children Dropdown Fix - no-cache
+const CACHE_NAME = 'familytreepro-v156-26-children-dropdown-fix-no-cache';
 
 self.addEventListener('install', event => { self.skipWaiting(); });
 self.addEventListener('activate', event => { event.waitUntil(caches.keys().then(keys => Promise.all(keys.map(k => caches.delete(k)))).then(() => self.clients.claim())); });
