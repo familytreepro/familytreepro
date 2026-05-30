5c94bffb9396f2e4de16815331316db916d2607a
familytreepro <alibahripor33@gmail.com>
2026-05-30 17:54:08 +0330

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-// FamilyTreePro V156.29 Photo Storage Clean Fix - no-cache
-const CACHE_NAME = 'familytreepro-v156-29-photo-storage-clean-fix-no-cache';
+// FamilyTreePro V156.30 Manual Spouse Fields - no-cache
+const CACHE_NAME = 'familytreepro-v156-30-manual-spouse-fields-no-cache';
 
 self.addEventListener('install', event => { self.skipWaiting(); });
 self.addEventListener('activate', event => { event.waitUntil(caches.keys().then(keys => Promise.all(keys.map(k => caches.delete(k)))).then(() => self.clients.claim())); });
