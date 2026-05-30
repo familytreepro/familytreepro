4dc4aff2d8d5b1794cbfc4388b266a8fe5cc4101
familytreepro <alibahripor33@gmail.com>
2026-05-30 13:57:57 +0330

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,22 +1,9 @@
-// FamilyTreePro V156.24 Children Clean Drawer - no-cache
-const CACHE_NAME = 'familytreepro-v156-24-children-clean-drawer-no-cache';
+// FamilyTreePro V156.25 Children Select Fix - no-cache
+const CACHE_NAME = 'familytreepro-v156-25-children-select-fix-no-cache';
 
-self.addEventListener('install', event => {
-  self.skipWaiting();
-});
-
-self.addEventListener('activate', event => {
-  event.waitUntil(
-    caches.keys()
-      .then(keys => Promise.all(keys.map(k => caches.delete(k))))
-      .then(() => self.clients.claim())
-  );
-});
-
+self.addEventListener('install', event => { self.skipWaiting(); });
+self.addEventListener('activate', event => { event.waitUntil(caches.keys().then(keys => Promise.all(keys.map(k => caches.delete(k)))).then(() => self.clients.claim())); });
 self.addEventListener('fetch', event => {
   if (event.request.method !== 'GET') return;
-  event.respondWith(
-    fetch(event.request, { cache: 'no-store' })
-      .catch(() => new Response('', { status: 503, statusText: 'Offline' }))
-  );
+  event.respondWith(fetch(event.request, { cache: 'no-store' }).catch(() => new Response('', { status: 503, statusText: 'Offline' })));
 });
