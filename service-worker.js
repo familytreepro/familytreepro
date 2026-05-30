8ffdd020c376122d40a4673ac4e251da4cc2153d
familytreepro <alibahripor33@gmail.com>
2026-05-30 17:24:05 +0330

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-// FamilyTreePro V156.28 Error Clean Guard - no-cache
-const CACHE_NAME = 'familytreepro-v156-28-children-dropdown-fix-no-cache';
+// FamilyTreePro V156.29 Photo Storage Clean Fix - no-cache
+const CACHE_NAME = 'familytreepro-v156-29-photo-storage-clean-fix-no-cache';
 
 self.addEventListener('install', event => { self.skipWaiting(); });
 self.addEventListener('activate', event => { event.waitUntil(caches.keys().then(keys => Promise.all(keys.map(k => caches.delete(k)))).then(() => self.clients.claim())); });
