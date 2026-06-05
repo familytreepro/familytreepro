4d281a8b483e667f3747b298b010a98b6e8208fa
familytreepro <alibahripor33@gmail.com>
2026-06-05 18:53:36 +0330

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 // FamilyTreePro V156.50 Shared Photo Cloud - network first with safe offline fallback
-const CACHE_NAME = 'familytreepro-v156-50-shared-photo-cloud';
+const CACHE_NAME = 'familytreepro-v156-52-shared-photo-cloud';
 const CORE_ASSETS = ['./', './index.html', './manifest.json'];
 self.addEventListener('install', event => {
   event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(CORE_ASSETS).catch(()=>{})).then(()=>self.skipWaiting()));
