430b3e0d9a421b38171d6799cf5668fc65f12cce
familytreepro <alibahripor33@gmail.com>
2026-06-06 14:02:59 +0330

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-// FamilyTreePro V156.91 F35 Lite Photo Engine
-const CACHE_NAME='familytreepro-v156-91-f35-lite-photo';
+// FamilyTreePro V156.92 F35 Lite Display Fix
+const CACHE_NAME='familytreepro-v156-92-f35-lite-display-fix';
 const CORE_ASSETS=['./','./index.html','./manifest.json'];
 self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE_NAME).then(cache=>cache.addAll(CORE_ASSETS).catch(()=>{})).then(()=>self.skipWaiting()))});
 self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE_NAME).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
