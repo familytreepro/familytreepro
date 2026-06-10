8e5f0901903ee6411e7ec6b24e898639598f18e6
familytreepro <alibahripor33@gmail.com>
2026-06-10 15:39:48 +0330

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-// FamilyTreePro V162 Mobile Firebase Debug
-const CACHE_NAME='familytreepro-v162-mobile-debug-fix';
+// FamilyTreePro V163 Mobile Auth Retry
+const CACHE_NAME='familytreepro-v163-mobile-auth-retry';
 const CORE_ASSETS=['./','./index.html','./manifest.json'];
 self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE_NAME).then(cache=>cache.addAll(CORE_ASSETS).catch(()=>{})).then(()=>self.skipWaiting()))});
 self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE_NAME).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
