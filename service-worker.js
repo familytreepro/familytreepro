1295596159c9a079d148ce8fae180c2ae4fc5941
familytreepro <alibahripor33@gmail.com>
2026-06-13 14:09:13 +0330

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-// FamilyTreePro V166 Mobile Auth Role Chain Fix
-const CACHE_NAME='familytreepro-v166-mobile-auth-role';
+// FamilyTreePro V167 Notification SMS Secure
+const CACHE_NAME='familytreepro-v167-notification-sms-secure';
 const CORE_ASSETS=['./','./index.html','./manifest.json'];
 self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE_NAME).then(cache=>cache.addAll(CORE_ASSETS).catch(()=>{})).then(()=>self.skipWaiting()))});
 self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE_NAME).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
