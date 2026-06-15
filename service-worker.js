9c0f33b9e71010a63f7602cc2f6343c1f2235fe0
familytreepro <alibahripor33@gmail.com>
2026-06-15 14:28:59 +0330

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-// FamilyTreePro V168 Auth Registry Repair
-const CACHE_NAME='familytreepro-v168-auth-registry-repair';
+// FamilyTreePro V169 Auth Registry Fixed
+const CACHE_NAME='familytreepro-v169-auth-fixed';
 const CORE_ASSETS=['./','./index.html','./manifest.json'];
 self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE_NAME).then(cache=>cache.addAll(CORE_ASSETS).catch(()=>{})).then(()=>self.skipWaiting()))});
 self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE_NAME).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
