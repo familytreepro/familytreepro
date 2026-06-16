6427a9d98a88dd1f0e8d3974170c8424463ab2ed
familytreepro <alibahripor33@gmail.com>
2026-06-16 16:42:12 +0330

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-// FamilyTreePro V200 Clean Unified
-const CACHE_NAME='familytreepro-v200-clean-unified';
+// FamilyTreePro V201 Clean Status
+const CACHE_NAME='familytreepro-v201-clean-status';
 const CORE_ASSETS=['./','./index.html','./manifest.json'];
 self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(CORE_ASSETS).catch(()=>{})).then(()=>self.skipWaiting()))});
 self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE_NAME).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
