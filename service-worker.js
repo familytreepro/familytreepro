44ea341946976833223352349cd585e4832a3744
familytreepro <alibahripor33@gmail.com>
2026-06-18 03:07:19 +0330

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-// FamilyTreePro V176 Clean Mobile Auth
-const CACHE_NAME='familytreepro-v176-clean-mobile-auth';
+// FamilyTreePro V177 Clean Auth Dashboard Repair
+const CACHE_NAME='familytreepro-v177-clean-auth-dashboard';
 const CORE_ASSETS=['./','./index.html','./manifest.json'];
 self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE_NAME).then(cache=>cache.addAll(CORE_ASSETS).catch(()=>{})).then(()=>self.skipWaiting()))});
 self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE_NAME).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
