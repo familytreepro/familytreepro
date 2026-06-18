b57f86d0cf7132adedde2d1cef3c6a129bb9226a
familytreepro <alibahripor33@gmail.com>
2026-06-18 17:40:53 +0330

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-// FamilyTreePro V182 Mobile Role Read Fix
-const CACHE_NAME='familytreepro-v182-mobile-role-read-fix';
+// FamilyTreePro V183 Auth Users Super Engine
+const CACHE_NAME='familytreepro-v183-auth-users-super-engine';
 const CORE_ASSETS=['./','./index.html','./manifest.json'];
 self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE_NAME).then(cache=>cache.addAll(CORE_ASSETS).catch(()=>{})).then(()=>self.skipWaiting()))});
 self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE_NAME).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
