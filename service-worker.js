b587efb26b4b8379501974211a50c89b6170ac0d
familytreepro <alibahripor33@gmail.com>
2026-06-20 02:17:41 +0330

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-// FamilyTreePro V187 Auth Users Readable Engine
-const CACHE_NAME='familytreepro-v187-auth-users-readable-engine';
+// FamilyTreePro V188 Unified Auth Engine
+const CACHE_NAME='familytreepro-v188-unified-auth-engine';
 const CORE_ASSETS=['./','./index.html','./manifest.json'];
 self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE_NAME).then(cache=>cache.addAll(CORE_ASSETS).catch(()=>{})).then(()=>self.skipWaiting()))});
 self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE_NAME).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
