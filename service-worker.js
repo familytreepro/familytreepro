fe2dd316f26faf1565c2053abb9943cd042301ca
familytreepro <alibahripor33@gmail.com>
2026-06-22 17:37:01 +0330

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-// FamilyTreePro V224 Backup Import Engine
-const CACHE_NAME='familytreepro-v224-backup-import-engine';
+// FamilyTreePro V225 People List Accordion
+const CACHE_NAME='familytreepro-v225-people-list-accordion';
 const CORE_ASSETS=['./','./index.html','./manifest.json'];
 self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE_NAME).then(cache=>cache.addAll(CORE_ASSETS).catch(()=>{})).then(()=>self.skipWaiting()))});
 self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE_NAME).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
