108457569214cb093a64a8706df9c8accff1133d
familytreepro <alibahripor33@gmail.com>
2026-06-22 16:26:31 +0330

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-// FamilyTreePro V221 Children Spouse Manager
-const CACHE_NAME='familytreepro-v221-multi-spouse-children-rows';
+// FamilyTreePro V222 Children Spouse Manager
+const CACHE_NAME='familytreepro-v222-person-profile-edit-nav';
 const CORE_ASSETS=['./','./index.html','./manifest.json'];
 self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE_NAME).then(cache=>cache.addAll(CORE_ASSETS).catch(()=>{})).then(()=>self.skipWaiting()))});
 self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE_NAME).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
