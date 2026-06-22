660436247073e4d852a81a874ece5fc3a7b5e766
familytreepro <alibahripor33@gmail.com>
2026-06-22 18:25:39 +0330

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-// FamilyTreePro V227 Smart Relation Engine
-const CACHE_NAME='familytreepro-v227-smart-relation-engine';
+// FamilyTreePro V228 Fast Tree Export
+const CACHE_NAME='familytreepro-v228-fast-tree-export';
 const CORE_ASSETS=['./','./index.html','./manifest.json'];
 self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE_NAME).then(cache=>cache.addAll(CORE_ASSETS).catch(()=>{})).then(()=>self.skipWaiting()))});
 self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE_NAME).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
