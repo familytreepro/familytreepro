e1b0d63aad623748f51779401efaf1b290137aca
familytreepro <alibahripor33@gmail.com>
2026-06-23 12:45:13 +0330

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-// FamilyTreePro V236 Tree Photo UI Single Engine
-const CACHE_NAME='familytreepro-v236-tree-photo-ui-single-engine';
+// FamilyTreePro V237 V228 Tree Engine Restore
+const CACHE_NAME='familytreepro-v237-v228-tree-engine-restore';
 const CORE_ASSETS=['./','./index.html','./manifest.json'];
 self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE_NAME).then(cache=>cache.addAll(CORE_ASSETS).catch(()=>{})).then(()=>self.skipWaiting()))});
 self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE_NAME).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
