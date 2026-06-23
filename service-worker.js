d6e033b13e9440f6093096279a9ddb64980b0197
familytreepro <alibahripor33@gmail.com>
2026-06-23 13:26:07 +0330

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-// FamilyTreePro V239 Ancestor Print Photo
-const CACHE_NAME='familytreepro-v239-ancestor-print-photo';
+// FamilyTreePro V240 Hide Women Tree Toggle
+const CACHE_NAME='familytreepro-v240-hide-women-tree';
 const CORE_ASSETS=['./','./index.html','./manifest.json'];
 self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE_NAME).then(cache=>cache.addAll(CORE_ASSETS).catch(()=>{})).then(()=>self.skipWaiting()))});
 self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE_NAME).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
