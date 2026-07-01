f86255b30541e8ee6c5f924f3e93c33e4cb39dd7
familytreepro <alibahripor33@gmail.com>
2026-07-02 00:13:58 +0330

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-// FamilyTreePro V253 Occasions Safe
-const CACHE_NAME='familytreepro-v253-occasions-safe';
+// FamilyTreePro V254-255 Birth Death Safe
+const CACHE_NAME='familytreepro-v254-255-birth-death-safe';
 const CORE_ASSETS=['./','./index.html','./manifest.json'];
 self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE_NAME).then(cache=>cache.addAll(CORE_ASSETS).catch(()=>{})).then(()=>self.skipWaiting()))});
 self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE_NAME).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
