c28639cb0e5af2babbd238e7dfff8b28543709d7
familytreepro <alibahripor33@gmail.com>
2026-07-01 20:35:12 +0330

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-// FamilyTreePro V249 Smart Profile
-const CACHE_NAME='familytreepro-v249-smart-profile';
+// FamilyTreePro V250 Stable Web
+const CACHE_NAME='familytreepro-v250-stable-web';
 const CORE_ASSETS=['./','./index.html','./manifest.json'];
 self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE_NAME).then(cache=>cache.addAll(CORE_ASSETS).catch(()=>{})).then(()=>self.skipWaiting()))});
 self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE_NAME).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
