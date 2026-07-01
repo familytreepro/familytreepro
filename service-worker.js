6bc6b6cc3945eba96d39f32fbebf9ec0767211e3
familytreepro <alibahripor33@gmail.com>
2026-07-01 18:23:09 +0330

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-// FamilyTreePro V245 Data Guard
-const CACHE_NAME='familytreepro-v245-data-guard';
+// FamilyTreePro V246 Photo Engine
+const CACHE_NAME='familytreepro-v246-photo-engine';
 const CORE_ASSETS=['./','./index.html','./manifest.json'];
 self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE_NAME).then(cache=>cache.addAll(CORE_ASSETS).catch(()=>{})).then(()=>self.skipWaiting()))});
 self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE_NAME).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
