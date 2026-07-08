024c6883ff7d42698c1e533193e787e7d8d1702a
familytreepro <alibahripor33@gmail.com>
2026-07-08 17:09:28 +0330

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-// FamilyTreePro V254 Spouse Dual Persian Only
-const CACHE_NAME='familytreepro-v254-spouse-dual-fa-only';
+// FamilyTreePro V254 Persian Only Delete Request Safe
+const CACHE_NAME='familytreepro-v254-fa-delete-request-safe';
 const CORE_ASSETS=['./','./index.html','./manifest.json'];
 self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE_NAME).then(cache=>cache.addAll(CORE_ASSETS).catch(()=>{})).then(()=>self.skipWaiting()))});
 self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE_NAME).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
